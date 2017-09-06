d27ff7c7b74000b190f9e6e057040db362e7fe19
kpack <kevinpackler@gmail.com>
2017-09-06 19:00:53 -0400

    change

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured, id) VALUES ('Everybody Loves Bacon', false, 1);
+INSERT INTO burgers (burger_name, devoured, id) VALUES ('Big Bun Theory', false, 2);
+INSERT INTO burgers (burger_name, devoured, id) VALUES ('Eggplosion', false, 3);
